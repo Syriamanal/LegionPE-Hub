d48b41e737111b23520601db53bba3f04559d61e
PEMapModder <pemapmodder1970@gmail.com>
2014-02-17 17:11:50 +0800

    message-less commit

--- Hub/PlayerProfile.php
+++ Hub/PlayerProfile.php
@@ -4,7 +4,11 @@ class PlayerProfile extends Config{
 	public function __construct($ign, $filename = "main"){
 		$ign = trim_player_ign($ign);
 		parent::__construct(hub_get_player_dir($ign)."$filename.yml", CONFIG_YAML, array(
-			
+			"statistics" => array(
+				
+			),
+			"personal points" => 0,
+			"team index" => -1
 		));
 	}
 }
