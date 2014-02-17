f2b94655b5e7421da1b3b2599630be613f39a55c
PEMapModder <pemapmodder1970@gmail.com>
2014-02-17 17:54:06 +0800

    message-less commit

--- Hub/MinigameCom.php
+++ Hub/MinigameCom.php
@@ -5,4 +5,20 @@ class MinigameCom{
 	public function __construct(HubPlugin $hub){
 		$this->hub = $hub;
 	}
+	public function init(){
+		$s = ServerAPI::request();
+		$s->addHandler("player.block.touch", array($this, "pmEvt"));
+		$s->addHandler("player.chat", array($this, "pmEvt"));
+		$s->addHandler("player.move", array($this, "pmEvt"));
+		$s->addHandler("player.interact", array($this, "pmEvt"));
+		$s->addHandler("player.quit", array($this, "pmEvt"));
+		$s->addHandler("player.death", array($this, "pmEvt"));
+		$s->addHandler("player.equipment.change", array($this, "pmEvt"));
+		$s->addHandler("tile.update", array($this, "pmEvt"));
+		$s->addHandler("entity.explosion", array($this, "pmEvt"));
+		$s->addHandler("entity.health.change", array($this, "pmEvt"));
+		$s->addHandler("console.command", array($this, "pmEvt"));
+		$s->addHandler("item.drop", array($this, "pmEvt"));
+		
+	}
 }
