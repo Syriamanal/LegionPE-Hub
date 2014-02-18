15a01deefe05f160d65e5f3693238d08dd1d9dab
PEMapModder <pemapmodder1970@gmail.com>
2014-02-18 17:24:59 +0800

    message-less commit

--- Hub/MinigameCom.php
+++ Hub/MinigameCom.php
@@ -17,12 +17,61 @@ class MinigameCom{
 		$s->addHandler("tile.update", array($this, "pmEvt"));
 		$s->addHandler("entity.explosion", array($this, "pmEvt"));
 		$s->addHandler("entity.health.change", array($this, "pmEvt"));
-		$s->addHandler("console.command", array($this, "pmEvt"));
 		$s->addHandler("item.drop", array($this, "pmEvt"));
 	}
 	public function pmEvt($data, $evt){
+		$s = ServerAPI::request();
+		$pa = $s->api->player;
+		$ea = $s->api->entity;
 		switch($evt){
-			
+			case "player.block.touch":
+				return $this->bpEvt($data["type"] == "break" ? "destroyBlock" : "useItem", $data["player"], array("item"=>$data["item"], "target"=>$data["target"]));
+			case "player.chat":
+				return $this->bpEvt("chat", $data["player"], $data["message"]);
+			case "player.move":
+				return $this->bpEvt("move", $data["player"]);
+			case "player.interact":
+				$attacker = $pa->getByEID($data["entity"]->eid);
+				$victim = $pa->getByEID($data["target"]->eid);
+				$e0 = $this->bpEvt("attack", $attacker, $data["target"]) === FALSE;
+				$e1 = FALSE;
+				if($victim instanceof Player)
+					$e1 = $this->bpEvt("attacked", $victim, $attacker) === FALSE;
+				$r = $e0 or $e1;
+				return !$r;
+			case "player.quit":
+				return $this->bpEvt("quit", $data);
+			case "player.death":
+				$victim = $data["player"];
+				$killer = $pa->getByEID($data["cause"]);
+				$e0 = $this->bpEvt("die", $victim, $data["cause"]) === FALSE;
+				$e1 = FALSE;
+				if($killer instanceof Player)
+					$e1 = $this->bpEvt("kill", $killer, $victim) === FALSE;
+				$r = $e0 or $e1;
+				return !$r;
+			case "player.equipment.change":
+				return $this->bpEvt("carryItem", $data["player"], $data["item"]);
+			case "tile.update":
+				return $this->bpEvt("placeSign", $data->data["creator"], $data);
+			case "entity.explosion":
+				return $this->bEvt("explode", $data->level, array("pos"=>$data["source"], "size"=>$data["size"]));
+			case "entity.health.change":
+				$victim = $pa->getByEID($data["eid"]);
+				$cause = $pa->getByEID($data["cause"]);
+				$old = $data["entity"]->getHealth();
+				$new = $data["health"];
+				$type = (($old > $new) ? "harm" : "heal");
+				$e0 = FALSE;
+				if($victim instanceof Player)
+					$e0 = $this->bpEvt($type."ed", $victim, array("cause" => $data["cause"], "delta" => abs($old - $new)));
+				$e1 = FALSE;
+				if($cause instanceof Player)
+					$e1 = $this->bpEvt($type, $cause, array("target" => $data["entity"], "delta" => abs($old - $new)));
+				$r = $e0 or $e1;
+				return !$r;
+			case "item.drop":
+				return $this->bEvt("dropItem", $data["level"], array("pos"=>(new Position($data["x"], $data["y"], $data["z"], $data["level"])), "item"=>$data["item"]));
 		}
 	}
 }
